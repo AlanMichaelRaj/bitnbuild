e032f71e4b77c2085f1098843319a28ff4ed559c
AlanMichaelRaj <140911023+AlanMichaelRaj@users.noreply.github.com>
2024-10-11 19:31:20 +0530

    Add files via upload

---
+++ Event.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    date: { type: Date, required: true },
+    seats: { type: Number, required: true },
+    description: { type: String },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+});
+
+module.exports = mongoose.model('Event', eventSchema);
